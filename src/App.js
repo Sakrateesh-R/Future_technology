9d737ffb3149a0f9341146e833a088eb2d1e9a7e
sathishkntrj <sathishkntrj@gmail.com>
2023-01-07 22:33:33 +0530

    Added New Project .jsfile
    Added and updated App js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './Components/HomePage';
 import Footer from './Components/Footer';
 import Navigation from './Components/Navigation';
 import About from './Components/About';
+import Project from './Components/Project';
 function App() {
   return (
     
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path = "/" element={<HomePage/>} />
           <Route path="/About" element={<About/>}/>
+          <Route path="/Projects" element={<Project/>}/>
         </Routes>
         <Footer/>
       </Router>
---
+++ src/Components/Project.js
@@ -0,0 +1,9 @@
+
+export default function Project() {  
+
+    return(
+        <div className="Project-Container">
+            <h1>Project 1 </h1>
+        </div>
+    )
+}
